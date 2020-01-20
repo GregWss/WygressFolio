aa32c64b03739cde81cf1e6bd7cda4932c17f649
Grégory Weiss <weiss.gregory48@gmail.com>
2020-01-20 15:35:18 -0500

    First commit and package fixing

---
+++ src/app/isstracker-carousel/isstracker-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-isstracker-carousel',
+  templateUrl: './isstracker-carousel.component.html',
+  styleUrls: ['./isstracker-carousel.component.scss']
+})
+export class ISSTrackerCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
